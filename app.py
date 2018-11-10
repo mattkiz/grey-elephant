83a620ff7373a5175006c33e45b18874b3c376bd
seanmark89 <seanmark89@gmail.com>
2018-11-10 14:20:11 -0600

    Updated home page, include background image, button and slogan.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Bootstrap(app)
 
 @app.route("/")
 def home():
-    return render_template("base_template.html")
+    return render_template("home.html")
 
 @app.route("/recipientinfo", methods=["GET"])
 def recipient_info():
