eb8d19757c6594916c8c8b63d4fcf16c412d6256
mattkiz <kizaric@wisc.edu>
2018-11-10 13:20:22 -0600

    Added templates and cleaned up routing

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.template_folder = "./template"
+app.template_folder = "./templates"
 Bootstrap(app)
 
 @app.route("/")
-def root():
-    return "Hello World!"
+def home():
+    return render_template("base_template.html")
 
-@app.route("/hack")
-def hack():
-    return render_template("index.html")
+@app.route("/recipientinfo", methods=["GET"])
+def recipient_info():
+    return "Rec_Info"
+
+@app.route("/recipientinfo", methods=["POST"])
+def recipient_info_post():
+    return "Rec_Info_Post"
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 app.run()
