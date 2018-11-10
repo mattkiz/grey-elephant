33dac08dc62984cf793da877ced21fbb822047a1
mattkiz <kizaric@wisc.edu>
2018-11-10 12:36:14 -0600

    Started on backend/frontend code

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+app.template_folder = "./template"
+Bootstrap(app)
 
 @app.route("/")
 def root():
     return "Hello World!"
 
+@app.route("/hack")
+def hack():
+    return render_template("index.html")
 app.run()
