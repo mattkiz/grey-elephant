5e584c69a58de1f39cb2a90a29894e22ff700d61
seanmark89 <seanmark89@gmail.com>
2018-11-11 00:19:49 -0600

    Updated home page, include background image, button and slogan.

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ Bootstrap(app)
 def home():
     return render_template("home.html")
 
+@app.route("/login")
+def login():
+
+    return render_template("home.html")
+
 @app.route("/recipientinfo/", methods=["GET"])
 def recipient_info():
     form = RecipientForm(request.form)
@@ -19,8 +24,31 @@ def recipient_info():
 @app.route("/recipientinfo/", methods=["POST"])
 def recipient_info_post():
     form = RecipientForm(request.form)
+
+    import smtplib
+
+    def sendemail(from_addr = 'greyelephantstaff@gmail.com', to_addr_list = [form.email.data],
+                  subject = 'Merry Gift Giving!', message = 'Click the link to receive a gift from your friend! Link: http://127.0.0.1:5000/login',
+                  login = 'greyelephantstaff@gmail.com', password = 'HackerFooBar123',
+                  smtpserver='smtp.gmail.com:587'):
+        print "function got called"
+
+        header = 'From: %s\n' % from_addr
+        header += 'To: %s\n' % ','.join(to_addr_list)
+        header += 'Subject: %s\n\n' % subject
+        message = header + message
+
+        server = smtplib.SMTP(smtpserver)
+        server.starttls()
+        server.login(login, password)
+        problems = server.sendmail(from_addr, to_addr_list, message)
+        server.quit()
+
+        return problems
+
     if form.validate():
-        data = scrape(form.instagram.data)
+        sendemail()
+        # data = scrape(form.instagram.data)
     else:
         return render_template("recipient_info_form.html", form=form, error=True)
     return str(form.data)
@@ -29,6 +57,9 @@ def recipient_info_post():
 def results():
     return render_template("results.html")
 
+@app.route("/aboutgreyelephant/")
+def aboutgreyelephant():
+    return render_template("aboutgreyelephant.html")
 
 if __name__ == "__main__":
     app.run()
